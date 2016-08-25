eb336795c26ca496dc693f7658db24c53273cb06
Fernando Via Canel <fernando.canel@klarna.com>
2016-08-25 15:01:37 +0200

    Fix the introductory slides

--- src/Slides/Introduction/index.jsx
+++ src/Slides/Introduction/index.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import Screen from 'Screen'
 import * as Title from '@klarna/ui/Title'
-import * as Paragraph from '@klarna/ui/Paragraph'
+// import * as Paragraph from '@klarna/ui/Paragraph'
 
-export default function Buttons () {
-  return (
-    <Screen>
-      <div>
-        <Title.Primary margins>@klarna/ui components</Title.Primary>
-        <Paragraph.Primary margins>Join #ui-workshop</Paragraph.Primary>
-      </div>
-    </Screen>
-  )
-}
+export default React.createClass({
+  getInitialState () {
+    return {
+      subSlide: 'main'
+    }
+  },
+
+  render () {
+    const getVisibility = (key) => ({
+      display: key === this.state.subSlide ? 'block' : 'none'
+    })
+
+    return (
+      <Screen>
+        <Title.Primary
+          onClick={() => this.setState({ subSlide: 'join' })}
+          style={getVisibility('main')}
+          margins>
+          @klarna/ui components
+        </Title.Primary>
+
+        <Title.Primary style={getVisibility('join')} margins>
+          Join #ui-workshop
+        </Title.Primary>
+      </Screen>
+    )
+  }
+})
--- src/Slides/WhyReact/index.jsx
+++ src/Slides/WhyReact/index.jsx
@@ -1,40 +1,51 @@
 import React from 'react'
 import Screen from 'Screen'
 import * as Title from '@klarna/ui/Title'
-import * as Paragraph from '@klarna/ui/Paragraph'
 import GistEmbed from 'react-gist-embed'
 import styles from './styles.css'
 
 export default React.createClass({
   getInitialState () {
     return {
-      subSlide: 'main'
+      current: 0
     }
   },
 
   render () {
-    const getVisibility = (key) => ({
-      display: key === this.state.subSlide ? 'block' : 'none'
-    })
+    return [
+      (<Screen
+        onClick={() => this.setState({ current: 1 })}>
+        <Title.Primary margins>
+          &lt;<strong className={styles.strong}>React</strong> butWhy='?' /&gt;
+        </Title.Primary>
+      </Screen>),
 
-    return (
-      <Screen
-        onClick={() => this.setState({ subSlide: 'gist' })}>
-        <div
-          style={getVisibility('main')}>
-          <Title.Primary margins>
-            &lt;<strong className={styles.strong}>React</strong> butWhy='?' /&gt;
-          </Title.Primary>
+      (<Screen
+        onClick={() => this.setState({ current: 2 })}>
+        <Title.Primary margins>
+          Easy.
+        </Title.Primary>
+      </Screen>),
 
-          <Paragraph.Primary className={styles.paragraph} margins>
-            Easy. Stateless. Standard.
-          </Paragraph.Primary>
-        </div>
-
-        <div style={getVisibility('gist')}>
+      (<Screen
+        onClick={() => this.setState({ current: 3 })}>
+        <div>
           <GistEmbed gistId='5bea62f8c89448335a4b75b5e826a2c5' />
         </div>
-      </Screen>
-    )
+      </Screen>),
+
+      (<Screen
+        onClick={() => this.setState({ current: 4 })}>
+        <Title.Primary margins>
+          Easy. Stateless.
+        </Title.Primary>
+      </Screen>),
+
+      (<Screen>
+        <Title.Primary margins>
+          Easy. Stateless. Standard.
+        </Title.Primary>
+      </Screen>)
+    ][this.state.current]
   }
 })
